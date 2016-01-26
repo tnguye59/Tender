f3b8b98249318e39e8e0911eae86a6ecc53f19a7
Daniel Suh <danielsuh20@gmail.com>
2016-01-25 18:24:26 -0800

    fixed edit personal questions

--- app/controllers/personal_questions_controller.rb
+++ app/controllers/personal_questions_controller.rb
@@ -12,6 +12,21 @@ class PersonalQuestionsController < ApplicationController
   	end 
   end
 
+  def edit
+    @question = PersonalQuestion.find_by(user_id: params[:id])
+    render 'edit'
+  end
+
+  def update
+     q = PersonalQuestion.find_by(user_id: params[:id])
+    if q.update(personal_params)
+      redirect_to '/user'
+    else
+      flash[:errors] = q.errors.full_messages
+      redirect_to :back
+    end
+  end
+
   private
   	def personal_params
   		params.require(:personal_question).permit(:q1,:q2,:q3, :user_id)
