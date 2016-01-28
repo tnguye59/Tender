401db98d7e15f1aa4974173408c41d0457faa4d3
Tommy <tnguye59@gmail.com>
2016-01-28 11:45:33 -0800

    add chat socket.io

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
 	var socket = io.connect("http://127.0.0.1:8000", {force_connection: true});
+
+	socket.on("welcome", function(){
+		$("#chatbox").append("<p>Has entered chatroom</p>")
+	})
 })
 
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,16 +6,16 @@ class CommentsController < ApplicationController
 	def create
 		message = Message.new(message: params[:message][:message], post_id: params[:message][:post_id], match_id: 2, user_id: params[:message][:user_id])
 		# message = Message.new(message_params)
-	    if message.save
-	      redirect_to :back
-	    else 
-	      flash[:errors] = message.errors.full_messages
-	      redirect_to :back
-	    end 
+	    # if message.save
+	    #   redirect_to :back
+	    # else 
+	    #   flash[:errors] = message.errors.full_messages
+	    #   redirect_to :back
+	    # end 
 	end
 
-	private
-		def message_params
-	      params.require(:message).permit(:message, :user_id, :match_id, :post_id)
-	    end
+	# private
+	# 	def message_params
+	#       params.require(:message).permit(:message, :user_id, :match_id, :post_id)
+	#     end
 end
