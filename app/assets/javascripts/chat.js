5fc45f9b9a1f3c46ea6900e8706f642f52cb1ac4
Tommy <tnguye59@gmail.com>
2016-01-28 11:59:16 -0800

    socket io

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
 	var socket = io.connect("http://127.0.0.1:8000", {force_connection: true});
 
-	socket.on("welcome", function(){
+	socket.on("welcome", function(response){
 		$("#chatbox").append("<p>Has entered chatroom</p>")
+	})
+
+	$("#chat-form").submit(function(){
+		var info = $("#textbox").val();
+		socket.emit("message", {message: info})
+		$("#textbox").val("");
+		return false;
 	})
 })
 
