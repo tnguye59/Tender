515d7ae9ab24cbc6ba0ce46c7869af6d3bc976c7
Tommy <tnguye59@gmail.com>
2016-01-27 19:05:57 -0800

    debugged

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,5 @@
 class Match < ActiveRecord::Base
 	belongs_to :user, :foreign_key =>"user_id", :class_name =>"User"
 	belongs_to :match, :foreign_key =>"match_id", :class_name => "User"
-<<<<<<< HEAD
-=======
 	has_many :messages, dependent: :destroy
->>>>>>> chat
 end
