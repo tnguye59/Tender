fe7965c521dd639b3d03d555b6731768d5fc5aad
Jessica Peng <jessicaspeng@gmail.com>
2016-01-28 13:16:03 -0800

    styling dashboard

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-	var socket = io.connect("http://127.0.0.1:8000", {force_connection: true});
+// $(document).ready(function(){
+// 	var socket = io.connect("http://127.0.0.1:8000", {force_connection: true});
 
-	socket.on("welcome", function(response){
-		$("#chatbox").append("<p>Has entered chatroom</p>")
-	})
+// 	socket.on("welcome", function(response){
+// 		$("#chatbox").append("<p>Has entered chatroom</p>")
+// 	})
 
-	$("#chat-form").submit(function(){
-		var info = $("#textbox").val();
-		socket.emit("message", {message: info})
-		$("#textbox").val("");
-		return false;
-	})
-})
+// 	$("#chat-form").submit(function(){
+// 		var info = $("#textbox").val();
+// 		socket.emit("message", {message: info})
+// 		$("#textbox").val("");
+// 		return false;
+// 	})
+// })
 
